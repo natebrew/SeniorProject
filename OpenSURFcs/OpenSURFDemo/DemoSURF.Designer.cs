2ba670b042ac052f137744698b08bb42b73f54c4
Jeremy McCurdy <jer.mccurdy@gmail.com>
2014-03-08 18:51:30 -0700

    changed a bunch

--- OpenSURFcs/OpenSURFDemo/DemoSURF.Designer.cs
+++ OpenSURFcs/OpenSURFDemo/DemoSURF.Designer.cs
@@ -28,46 +28,65 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.pbMainPicture = new System.Windows.Forms.PictureBox();
-      this.btnRunSurf = new System.Windows.Forms.Button();
-      ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // pbMainPicture
-      // 
-      this.pbMainPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                  | System.Windows.Forms.AnchorStyles.Left)
-                  | System.Windows.Forms.AnchorStyles.Right)));
-      this.pbMainPicture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-      this.pbMainPicture.Location = new System.Drawing.Point(12, 12);
-      this.pbMainPicture.Name = "pbMainPicture";
-      this.pbMainPicture.Size = new System.Drawing.Size(685, 458);
-      this.pbMainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-      this.pbMainPicture.TabIndex = 0;
-      this.pbMainPicture.TabStop = false;
-      // 
-      // btnRunSurf
-      // 
-      this.btnRunSurf.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-      this.btnRunSurf.Location = new System.Drawing.Point(281, 481);
-      this.btnRunSurf.Name = "btnRunSurf";
-      this.btnRunSurf.Size = new System.Drawing.Size(134, 39);
-      this.btnRunSurf.TabIndex = 1;
-      this.btnRunSurf.Text = "&Run OpenSURF";
-      this.btnRunSurf.UseVisualStyleBackColor = true;
-      this.btnRunSurf.Click += new System.EventHandler(this.btnRunSurf_Click);
-      // 
-      // DemoSURF
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(708, 526);
-      this.Controls.Add(this.btnRunSurf);
-      this.Controls.Add(this.pbMainPicture);
-      this.Name = "DemoSURF";
-      this.Text = "DemoSURF";
-      ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).EndInit();
-      this.ResumeLayout(false);
+            this.pbMainPicture = new System.Windows.Forms.PictureBox();
+            this.btnRunSurf = new System.Windows.Forms.Button();
+            this.pbMain2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMainPicture
+            // 
+            this.pbMainPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMainPicture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbMainPicture.Location = new System.Drawing.Point(12, 12);
+            this.pbMainPicture.Name = "pbMainPicture";
+            this.pbMainPicture.Size = new System.Drawing.Size(442, 422);
+            this.pbMainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMainPicture.TabIndex = 0;
+            this.pbMainPicture.TabStop = false;
+            // 
+            // btnRunSurf
+            // 
+            this.btnRunSurf.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnRunSurf.Location = new System.Drawing.Point(323, 440);
+            this.btnRunSurf.Name = "btnRunSurf";
+            this.btnRunSurf.Size = new System.Drawing.Size(134, 39);
+            this.btnRunSurf.TabIndex = 1;
+            this.btnRunSurf.Text = "&Run OpenSURF";
+            this.btnRunSurf.UseVisualStyleBackColor = true;
+            this.btnRunSurf.Click += new System.EventHandler(this.btnRunSurf_Click);
+            // 
+            // pbMain2
+            // 
+            this.pbMain2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMain2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbMain2.Location = new System.Drawing.Point(465, 12);
+            this.pbMain2.Name = "pbMain2";
+            this.pbMain2.Size = new System.Drawing.Size(345, 422);
+            this.pbMain2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMain2.TabIndex = 2;
+            this.pbMain2.TabStop = false;
+            // 
+            // DemoSURF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(822, 485);
+            this.Controls.Add(this.pbMain2);
+            this.Controls.Add(this.btnRunSurf);
+            this.Controls.Add(this.pbMainPicture);
+            this.Name = "DemoSURF";
+            this.Text = "DemoSURF";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain2)).EndInit();
+            this.ResumeLayout(false);
 
     }
 
@@ -75,6 +94,7 @@
 
     private System.Windows.Forms.PictureBox pbMainPicture;
     private System.Windows.Forms.Button btnRunSurf;
+    private System.Windows.Forms.PictureBox pbMain2;
   }
 }
 
--- OpenSURFcs/OpenSURFDemo/DemoSURF.cs
+++ OpenSURFcs/OpenSURFDemo/DemoSURF.cs
@@ -11,75 +11,229 @@ using OpenSURFcs;
 
 namespace OpenSURFDemo
 {
-  public partial class DemoSURF : Form
-  {
-    public DemoSURF()
+    public partial class DemoSURF : Form
     {
-      InitializeComponent();
-    }
+        public DemoSURF()
+        {
+            InitializeComponent();
+        }
 
-    List<IPoint> ipts = new List<IPoint>();
+        List<IPoint> ipts = new List<IPoint>();
+        List<IPoint> ipts2 = new List<IPoint>();
 
-    private void btnRunSurf_Click(object sender, EventArgs e)
-    {
-      OpenFileDialog openFileDialog = new OpenFileDialog();
-      openFileDialog.ShowDialog();
-      string pathToFile = openFileDialog.FileName;
+        private void btnRunSurf_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            string pathToFile = openFileDialog.FileName;
 
-      Stopwatch watch = new Stopwatch();
-      watch.Start();
+            OpenFileDialog open = new OpenFileDialog();
+            open.ShowDialog();
+            string path = open.FileName;
 
-      try
-      {
-        // Load an Image
-        Bitmap img = new Bitmap(pathToFile);
-        pbMainPicture.Image = img;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            try
+            {
+                // Load an Image
+                Bitmap img = new Bitmap(pathToFile);
+                Bitmap img2 = new Bitmap(path);
+                pbMainPicture.Image = img;
+                pbMain2.Image = img2;
         
-        // Create Integral Image
-        IntegralImage iimg = IntegralImage.FromImage(img);
+                // Create Integral Image
+                IntegralImage iimg = IntegralImage.FromImage(img);
+                IntegralImage iimg2 = IntegralImage.FromImage(img2);
         
-        // Extract the interest points
-        ipts = FastHessian.getIpoints(0.0002f, 5, 2, iimg);
+                // Extract the interest points
+                ipts = FastHessian.getIpoints(0.0002f, 5, 2, iimg);
+                ipts2 = FastHessian.getIpoints(0.0002f, 5, 2, iimg2);
 
-        // Describe the interest points
-        SurfDescriptor.DecribeInterestPoints(ipts, false, false, iimg);
+                // Describe the interest points
+                SurfDescriptor.DecribeInterestPoints(ipts, false, false, iimg);
+                SurfDescriptor.DecribeInterestPoints(ipts2, false, false, iimg2);
 
-        // Draw points on the image
-        PaintSURF(img, ipts);
-      }
-      catch
-      {
+                //Do the comparison
 
-      }
+                List<IPoint> PointPairs = new List<IPoint>();
 
-      watch.Stop();
-      this.Text = "DemoSURF - Elapsed time: " + watch.Elapsed + 
-                  " for " + ipts.Count + "points";
-    }
+                //first attempt
+                //findPairs(ipts, ipts2, PointPairs);
 
-    private void PaintSURF(Bitmap img, List<IPoint> ipts)
-    {
-      Graphics g = Graphics.FromImage(img);
+                getMatches(ipts, ipts2, PointPairs);
+
+                Console.WriteLine("pairs size" + PointPairs.Count);
+
+                // Draw points on the image
+                PaintSURF(img, ipts);
+                PaintSURF(img2, ipts2);
+
+            }
+            catch
+            {
+
+            }
+
+            watch.Stop();
+            this.Text = "DemoSURF - Elapsed time: " + watch.Elapsed + 
+                        " for " + ipts.Count + "points";
+        }
+
+        private void PaintSURF(Bitmap img, List<IPoint> ipts)
+        {
+            Graphics g = Graphics.FromImage(img);
       
-      Pen redPen = new Pen(Color.Red);
-      Pen bluePen = new Pen(Color.Blue);
-      Pen myPen;
+            Pen redPen = new Pen(Color.Red);
+            Pen bluePen = new Pen(Color.Blue);
+            Pen myPen;
 
-      foreach (IPoint ip in ipts)
-      {
-        int S = 2 * Convert.ToInt32(2.5f * ip.scale);
-        int R = Convert.ToInt32(S / 2f);
+            foreach (IPoint ip in ipts)
+            {
+                int S = 2 * Convert.ToInt32(2.5f * ip.scale);
+                int R = Convert.ToInt32(S / 2f);
 
-        Point pt = new Point(Convert.ToInt32(ip.x), Convert.ToInt32(ip.y));
-        Point ptR = new Point(Convert.ToInt32(R * Math.Cos(ip.orientation)),
-                     Convert.ToInt32(R * Math.Sin(ip.orientation)));
+                Point pt = new Point(Convert.ToInt32(ip.x), Convert.ToInt32(ip.y));
+                Point ptR = new Point(Convert.ToInt32(R * Math.Cos(ip.orientation)), Convert.ToInt32(R * Math.Sin(ip.orientation)));
 
-        myPen = (ip.laplacian > 0 ? bluePen : redPen);
+                myPen = (ip.laplacian > 0 ? bluePen : redPen);
 
-        g.DrawEllipse(myPen, pt.X - R, pt.Y - R, S, S);
-        g.DrawLine(new Pen(Color.FromArgb(0, 255, 0)), new Point(pt.X, pt.Y), new Point(pt.X + ptR.X, pt.Y + ptR.Y));
-      }
-    }
+                g.DrawEllipse(myPen, pt.X - R, pt.Y - R, S, S);
+                g.DrawLine(new Pen(Color.FromArgb(0, 255, 0)), new Point(pt.X, pt.Y), new Point(pt.X + ptR.X, pt.Y + ptR.Y));
+            }
+        }
 
-  }  // DemoApp
+        // compare SURF Descriptors
+        private double compareSURFDescriptors(float[] d1, float[] d2, double best, int length)
+        {
+            double total_cost = 0;
+            //assert( length % 4 == 0 );
+            for (int i = 0; i < length; i += 4)
+            {
+                double t0 = d1[i] - d2[i];
+                double t1 = d1[i + 1] - d2[i + 1];
+                double t2 = d1[i + 2] - d2[i + 2];
+                double t3 = d1[i + 3] - d2[i + 3];
+                total_cost += t0 * t0 + t1 * t1 + t2 * t2 + t3 * t3;
+                if (total_cost > best)
+                    break;
+            }
+
+            //Console.WriteLine("compare des return = " + total_cost);
+
+            return total_cost;
+        }
+
+        // naive Nearest Neighbor
+        private int nNN(float[] vec, int length, int laplacian, List<IPoint> ipts)
+        {
+            int i, neighbor = -1;
+            double d, dist1 = 1e6, dist2 = 1e6;
+
+            foreach (IPoint ip in ipts)
+            {
+                for (i = 0; i < ip.descriptorLength; ++i)
+                {
+                    if (laplacian != ip.laplacian)
+                        continue;
+                    d = compareSURFDescriptors(vec, ip.descriptor, dist2, length);
+                    if (d < dist1)
+                    {
+                        dist2 = dist1;
+                        dist1 = d;
+                        neighbor = i;
+                    }
+                    else if (d < dist2)
+                    {
+                        dist2 = d;
+                    }
+                }
+            }
+
+            //Console.WriteLine("dist1 = " + dist1);
+            //Console.WriteLine("dist2 = " + dist2);
+            //Console.WriteLine("Nei = " + neighbor);
+
+            if (dist1 < 0.5*dist2)
+            {
+                return neighbor;
+            }
+            
+            return -1;
+        }
+
+        // find key pairs of points
+        void findPairs(List<IPoint> ipts, List<IPoint> ipts2, List<int> pairs)
+        {
+            int i;
+            foreach (IPoint ip in ipts)
+            {
+                for (i = 0; i < ip.descriptorLength; ++i)
+                {
+                    // Nearest Neighbor
+                    int nn = nNN(ip.descriptor, ip.descriptorLength, ip.laplacian, ipts2);
+                    if (nn >= 0)
+                    {
+                        pairs.Add(i);
+                        pairs.Add(nn);
+                    }
+                }
+            }
+
+        }
+
+
+        public double subtract(IPoint i1, IPoint i2)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < i1.descriptorLength; ++i)
+            {
+                sum += (i1.descriptor[i] - i2.descriptor[i]) * (i1.descriptor[i] - i2.descriptor[i]);
+            }
+
+            return Math.Sqrt(sum);
+        }
+
+        void getMatches(List<IPoint> ipts, List<IPoint> ipts2, List<IPoint> matches)
+        {
+            double dist, d1, d2;
+            IPoint match = null;
+            matches.Clear();
+
+            d1 = d2 = Double.MaxValue;
+
+            for (int i = 0; i < ipts.Count(); i++)
+            {
+                d1 = d2 = Double.MaxValue;
+
+                for (int j = 0; j < ipts2.Count(); j++)
+                {
+                    dist = subtract(ipts.ElementAt(i), ipts2.ElementAt(j));
+
+                    if (dist < d1)
+                    {
+                        d2 = d1;
+                        d1 = dist;
+                        match = ipts2.ElementAt(j);
+                    }
+                    else if (dist < d2)
+                    {
+                        d2 = dist;
+                    }
+                }
+
+                //Console.WriteLine("d1 = " + d1);
+                //Console.WriteLine("d2 = " + d2);
+                //Console.WriteLine("/ = " + d1 / d2);
+
+                if (d1 / d2 < 0.72)
+                {
+                    //Console.WriteLine("Match Found");
+                    matches.Add(match);
+                }
+            }
+        }
+
+    }  // DemoApp
 } // OpenSURFDemo
